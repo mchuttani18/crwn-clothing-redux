8e830d6a4a4b5e85ec2357cb0e299123765ae0e0
Milan <mchuttani18@gmail.com>
2020-01-26 23:23:22 -0500

    added redux users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCv8dyunLZ2EFNiz40nQBa8gMvSxCOuLmE",
+  authDomain: "crwn-clothing-db-dbe39.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-db-dbe39.firebaseio.com",
+  projectId: "crwn-clothing-db-dbe39",
+  storageBucket: "crwn-clothing-db-dbe39.appspot.com",
+  messagingSenderId: "731427439268",
+  appId: "1:731427439268:web:e88f79d4ccfe646a0f9c31",
+  measurementId: "G-MQ19VGXJXK"
 };
 
 firebase.initializeApp(config);
